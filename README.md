f9a741c03966328ce527f94f06b86e5bdc348b57
007revad <39733752+007revad@users.noreply.github.com>
2022-11-21 14:27:25 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Synology_Config_Backup
+ Backup and export your Synology DSM configuration.
+
+### Description
+
+This is a bash script to backup a Synology's System Config, and optionally copy it via SCP to another server.
+
+The script was written to work on DSM 7.x and DSM 6.x (though it has only been tested on DSM 6).
+
+#### What the script does:
+
+* Gets your Synology's hostname and the date for use in the backup filename.
+  * The Synology's hostname is included in the backup's filename in case you have multiple Synology's.
+* Checks that the script is running as root.
+* Checks that your specified backup location exists.
+* Exports the system config to your specified folder.
+* If REMOTE_BACKUP=yes copies the backup to the specified remote server.
+
+**Example of the backup's auto-generated filename:** DISKSTATION_2022-11-07_0600.dss
+
+### Settings
+
+You need to set **TARGET_DIR=** near the top of the script (below the header). Set it to the location where you want the backup saved to. 
+
+You also need to set **REMOTE_BACKUP=** to yes if you want to copy the backup to a remote server.
+
+**For example:**
+
+```YAML
+TARGET_DIR="/volume1/Backups/NAS_System_Backups/DiskStation"
+REMOTE_BACKUP=yes
+```
+
+#### Extra settings for copying the backup to a remote server
+
+If you set REMOTE_BACKUP=yes you also need to set the REMOTE_PORT, REMOTE_IP, REMOTE_DIR, LOCAL_USER and REMOTE_USER to suit your setup.
+
+**For example:**
+
+```YAML
+REMOTE_PORT=22
+REMOTE_IP=192.168.0.100
+REMOTE_DIR="/volume1/Backups/NAS_System_Backups/DiskStation"
+LOCAL_USER=Bob
+REMOTE_USER=Bob
+```
+
+### Requirements
+
+If you want to schedule the script you run unattended, and have the script copy the backup to another server you need to have SSH keys setup so SCP can access the remote server without entering the user's password.
+
+**Note:** Due to some of the commands used **this script needs to be run as root, or be scheduled to run as root**.
---
+++ synology_config_backup.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+#--------------------------------------------------------------------------
+# Backup Synology system configuration and copy backup to another NAS
+#      Must be run as root or scheduled to run as root
+#
+# Tested on: DSM 6.2.4-25556 Update 6
+#
+# Author: 007revad
+# Date/Version: 2022-11-05 v5.0
+#
+# Gist on Github: https://gist.github.com/007revad
+# Script verified at https://www.shellcheck.net/
+#--------------------------------------------------------------------------
+
+# Set where to save the exported configuration file
+#TARGET_DIR="/volume1/Backups/NAS_System_Backups/$( hostname )"
+TARGET_DIR="/volume1/Backups/NAS_System_Backups/DiskStation"
+
+
+# Set to yes to enable remote backup. Anything else = no
+REMOTE_BACKUP=yes
+
+# Where to copy the exported configuration file to
+REMOTE_PORT=22
+REMOTE_IP=
+REMOTE_DIR="/volume1/Backups/NAS_System_Backups/DiskStation"
+
+# Local and remote users with SSH key setup
+LOCAL_USER=
+REMOTE_USER=
+
+
+#--------------------------------------------------------------------------
+#				 Nothing below here should need changing
+#--------------------------------------------------------------------------
+
+# Set backup filename
+
+# Append date and time to backup file
+FILE_NAME="$( hostname )_$( date +%F_%H%M ).dss"
+
+
+#--------------------------------------------------------------------------
+# Check that script is running as root
+
+if [[ $( whoami ) != "root" ]]; then
+	echo
+	echo ERROR: This script must be run as root!
+	echo ERROR: "$( whoami )" is not root. Aborting.
+	echo
+	# Abort script because it isn't being run by root
+	exit 255
+fi
+#echo "Script running as $( whoami )" #debug
+
+
+#--------------------------------------------------------------------------
+# Export Synology configuration to a Synology directory
+
+echo "Starting backup of Synology configuration on $( hostname )"
+echo
+
+if [[ ! -d $TARGET_DIR ]]; then
+	echo "Backup path does not exist:"
+	echo "${TARGET_DIR}"
+	exit 255
+fi
+#echo "${TARGET_DIR} exists." #debug
+
+cd "${TARGET_DIR}"
+/usr/syno/bin/synoconfbkp export --filepath="${TARGET_DIR}/${FILE_NAME}"
+
+# Check exported file exists
+if [[ ! -f ${TARGET_DIR}/${FILE_NAME} ]]; then
+	echo "Backup file does not exist:"
+	echo "${TARGET_DIR}/${FILE_NAME}"
+	exit 255
+else
+	#echo "Synology configuration exported to $FILE_NAME on $( hostname )"
+	#echo "Exported Synology configuration on $( hostname )"
+	echo "Export successful on $( hostname )"
+fi
+
+
+#--------------------------------------------------------------------------
+# Copy backup to remote NAS
+
+# Get remote NAS hostname
+REMOTE_HOST=$(nmblookup -A $REMOTE_IP | sed -n 2p | cut -d ' ' -f1)
+REMOTE_HOST="${REMOTE_HOST:1}"
+
+if [[ $REMOTE_BACKUP == "yes" ]]; then
+	#echo "Copying backup to other device"
+	if [[ $REMOTE_HOST ]]; then
+		echo
+		echo "Copying backup to ${REMOTE_HOST}"
+	else
+		echo
+		echo "Copying backup to ${REMOTE_IP}"
+	fi
+	# Push backup to other device (safer for other device to pull backup from read only share)
+	sudo -u "${LOCAL_USER}" scp -P "${REMOTE_PORT}" "${TARGET_DIR}/${FILE_NAME}" "${REMOTE_USER}@${REMOTE_IP}:${REMOTE_DIR}/"
+fi
+
+
+#--------------------------------------------------------------------------
+# Finished
+
+echo
+#echo "Settings backup completed."
+echo "Synology configuration backup complete"
+
+exit
+
